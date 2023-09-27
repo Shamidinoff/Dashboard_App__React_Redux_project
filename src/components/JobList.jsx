2c2c3b045c32d88382409521bd5a4e908fa3df8c
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-09-27 22:28:17 +0600

    Dashboard App - React Redux project

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,13 +1,21 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { JobPosition } from './JobPosition';
 import { selectAllPositions } from 'store/positions/position-selectors';
 
+import { addFilter } from 'store/filters/filter-actions';
+
 const JobList = () => {
-  const positions = useSelector(selectAllPositions)
+  const dispatch = useDispatch();
+  const positions = useSelector(selectAllPositions);
+
+  const handleAddFilter = (filter) => {
+    dispatch(addFilter(filter));
+  }
+
   return (
     <div className='job-list'>
       {positions.map(item => (
-        <JobPosition key={item.id} {...item} />
+        <JobPosition key={item.id} handleAddFilter={handleAddFilter} {...item} />
       ))}
     </div>
   )
--- src/store/filters/filter-reducer.js
+++ src/store/filters/filter-reducer.js
@@ -3,7 +3,10 @@ import { ADD_FILTER, REMOVE_FILTER, CLEAR_FILTER } from "./filter-actions";
 export const filterReducer = (state = [], action) => {
   switch (action.type) {
     case ADD_FILTER: {
-      return [...state, action.filter];
+      if (!state.includes(action.filter)) {
+        return [...state, action.filter];
+      }
+      return state;
     }
     case REMOVE_FILTER: {
       return state.filter((item) => item !== action.filter);
