d913a3b113c076c4ecb0009f6afd797f89e7faec
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-09-27 22:47:04 +0600

    Dashboard App - React Redux project

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,12 +1,14 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { JobPosition } from './JobPosition';
-import { selectAllPositions } from 'store/positions/position-selectors';
+import { selectVisiblePositions } from 'store/positions/position-selectors';
+import { selectFilters } from 'store/filters/filter-selectors';
 
 import { addFilter } from 'store/filters/filter-actions';
 
 const JobList = () => {
   const dispatch = useDispatch();
-  const positions = useSelector(selectAllPositions);
+  const currentFilters = useSelector(selectFilters);
+  const positions = useSelector((state) => selectVisiblePositions(state, currentFilters));
 
   const handleAddFilter = (filter) => {
     dispatch(addFilter(filter));
@@ -15,7 +17,10 @@ const JobList = () => {
   return (
     <div className='job-list'>
       {positions.map(item => (
-        <JobPosition key={item.id} handleAddFilter={handleAddFilter} {...item} />
+        <JobPosition
+          key={item.id}
+          handleAddFilter={handleAddFilter}
+          {...item} />
       ))}
     </div>
   )
