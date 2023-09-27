43027aadffbca0bfe5adfa1a232c307360317c3f
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-09-27 21:04:47 +0600

    Dashboard App - React Redux project

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,14 +1,16 @@
-import data from '../mock/data.json';
+import { useSelector } from 'react-redux';
 import { JobPosition } from './JobPosition';
+import { selectAllPositions } from 'store/positions/position-selectors';
 
 const JobList = () => {
+  const positions = useSelector(selectAllPositions)
   return (
     <div className='job-list'>
-      {data.map(item => (
+      {positions.map(item => (
         <JobPosition key={item.id} {...item} />
       ))}
     </div>
   )
 }
 
-export {JobList};
+export { JobList };
