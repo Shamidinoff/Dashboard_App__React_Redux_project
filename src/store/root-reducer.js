ce8488ba0a2802db4413cc736db034e6285066d4
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-09-27 21:28:36 +0600

    Dashboard App - React Redux project

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { positionReducer } from "./positions/position-reducer";
+import { filterReducer } from "./filters/filter-reducer";
 
 export const rootReducer = combineReducers({
   positions: positionReducer,
+  filters: filterReducer,
 });
